ba4703455d99b75ad6171b80b0207137ff4239b8
obaidmuneer <obaidmuneer55@gmail.com>
2022-09-15 23:06:35 +0500

    prayer timing v1.0

---
+++ js/app.js
@@ -0,0 +1,61 @@
+// let bgImage = document.querySelector('.container')
+function getData() {
+    let city = document.querySelector('#city').value
+    let footer = document.querySelector('.footer')
+    let apiKey = '55ff5b9f1aa556e25d9767c01329b185'
+    footer.innerHTML = ''
+
+    prayerTime(city)
+    // forecast(city, apiKey)
+
+}
+
+function prayerTime(city) {
+    // let prayerTimeApi = `https://dailyprayer.abdulrcs.repl.co/api/${city}`
+    let prayerTimeApi = `https://dailyprayer.abdulrcs.repl.co/api/karachi`
+    axios.get(prayerTimeApi)
+        .then(
+            function (res) {
+                // handleBackground(bgImage)
+                console.log(res.data);
+                document.querySelector('.city').innerHTML = `${res.data.city}`
+
+                let today = res.data.today
+                for (const key in today) {
+                    console.log(`${key}: ${today[key]}`);
+                    let item  = `<div class="left"><div class="color"></div><div class="name" >${key}</div></div> <div class="time">${today[key]}</div>`
+                    document.querySelector('#result').innerHTML += `<li>${item}</li>`
+                }
+
+            }
+        )
+        .catch(
+            function (err) {
+                console.log(err);
+                // document.querySelector('.city').innerHTML = err.response.data.message
+            }
+        )
+
+}
+prayerTime()
+
+function handleBackground(bgImage) {
+    let d = new Date()
+    if (d.getHours() < 12) {
+        console.log(d.getHours());
+        bgImage.style.background = "url('./img/rising.jpg') no-repeat center"
+    } else if (d.getHours() < 15) {
+        console.log(d.getHours());
+        bgImage.style.background = "url('./img/noon.jpg') no-repeat center"
+    } else if (d.getHours() < 20) {
+        console.log(d.getHours());
+        bgImage.style.background = "url('./img/set.jpg') no-repeat center"
+    } else {
+        console.log(d.getHours());
+        bgImage.style.background = "url('./img/night.jpg') no-repeat center"
+        document.querySelector('body').style.backgroundColor = '#000a12'
+    }
+    bgImage.style.backgroundSize = 'cover'
+    document.querySelector('#city').style.opacity = 0.5
+    document.querySelector('button').style.opacity = 0.5
+}
