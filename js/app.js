0d593561b8433adefe359b24a4250e0a5c40eaaf
obaidmuneer <obaidmuneer55@gmail.com>
2022-09-17 21:39:43 +0500

    added loading animation and fixed some bug

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
-// let bgImage = document.querySelector('.container')
 function getData() {
     let city = document.querySelector('#city').value
     document.querySelector('#result').innerHTML = ''
+    document.querySelector('.loader').style.display = 'block'
+    document.querySelector('.btn').disabled = true
+
 
     prayerTime(city)
 
@@ -12,17 +14,17 @@ function prayerTime(city) {
     axios.get(prayerTimeApi)
         .then(
             function (res) {
-                // handleBackground(bgImage)
                 console.log(res.data);
                 document.querySelector('.city').innerHTML = `${res.data.city}`
 
                 let today = res.data.today
                 for (const key in today) {
                     console.log(`${key}: ${today[key]}`);
-                    let item  = `<div class="left"><div class="color"></div><div class="name" >${key}</div></div> <div class="time">${today[key]}</div>`
+                    let item = `<div class="left"><div class="color"></div><div class="name" >${key}</div></div> <div class="time">${today[key]}</div>`
                     document.querySelector('#result').innerHTML += `<li>${item}</li>`
                 }
-
+                document.querySelector('.loader').style.display = 'none'
+                document.querySelector('.btn').disabled = false
             }
         )
         .catch(
@@ -32,25 +34,4 @@ function prayerTime(city) {
             }
         )
 
-}
-
-function handleBackground(bgImage) {
-    let d = new Date()
-    if (d.getHours() < 12) {
-        console.log(d.getHours());
-        bgImage.style.background = "url('./img/rising.jpg') no-repeat center"
-    } else if (d.getHours() < 15) {
-        console.log(d.getHours());
-        bgImage.style.background = "url('./img/noon.jpg') no-repeat center"
-    } else if (d.getHours() < 20) {
-        console.log(d.getHours());
-        bgImage.style.background = "url('./img/set.jpg') no-repeat center"
-    } else {
-        console.log(d.getHours());
-        bgImage.style.background = "url('./img/night.jpg') no-repeat center"
-        document.querySelector('body').style.backgroundColor = '#000a12'
-    }
-    bgImage.style.backgroundSize = 'cover'
-    document.querySelector('#city').style.opacity = 0.5
-    document.querySelector('button').style.opacity = 0.5
 }
